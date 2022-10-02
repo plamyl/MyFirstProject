829ea498b3d353d2ff7a6a3f4b8ce89570154464
plamyl <114296737+plamyl@users.noreply.github.com>
2022-10-02 19:39:52 +0300

    Change in Vowels_Sum.py

---
+++ For_Loop_Lab/Vowels_Sum.py
@@ -0,0 +1,16 @@
+text = input()
+total = 0
+
+for ch in text:
+    if ch == "a":
+        total += 1
+    elif ch == "e":
+        total += 2
+    elif ch == "i":
+        total += 3
+    elif ch == "o":
+        total += 4
+    elif ch == "u":
+        total += 5
+
+print(total)
