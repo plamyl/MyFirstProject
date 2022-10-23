725b562378f9a3d0ad7640c92ad09c8aae8e92d7
plamyl <114296737+plamyl@users.noreply.github.com>
2022-10-23 15:00:27 +0300

    Change in Max_number.py

---
+++ While_Loop/While _Loop_Lab/Max_number.py
@@ -0,0 +1,12 @@
+import sys
+
+max_num = -sys.maxsize
+
+number = input()
+
+while number != "Stop":
+    number = int(number)
+    if number > max_num:
+        max_num = number
+    number = input()
+print(max_num)
